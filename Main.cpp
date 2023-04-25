2118bdfe2512b461002519dfdbeceb2a0f206cb1
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-04-24 21:42:44 -0400

    Test

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+int main(){
+    printf("hola mundo");
+}
+
+
+using namespace std;
