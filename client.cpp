79f5dae917d4d13f817ad3c11746b690750d5096
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-05-28 14:45:09 -0400

    class client

--- client.cpp
+++ client.cpp
@@ -3,131 +3,114 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdexcept>
 
-#define TAM_BUFFER 2048
-const char* SERVER_IP = "127.0.0.1";
-const int PORT = 8080;
+#define TAM_BUFFER 50000
 
-// Función para recibir datos del servidor
-std::string recibirDatos(int socket) {
-    char buffer[TAM_BUFFER];
-    std::string datosRecibidos;
-    int bytesRecibidos;
+class ClienteSocket {
+private:
+    int socketCliente;
+    struct sockaddr_in servidorDir;
 
-    do {
-        bytesRecibidos = recv(socket, buffer, TAM_BUFFER - 1, 0);
-        if (bytesRecibidos == -1) {
-            throw std::runtime_error("Error al recibir datos del servidor");
+public:
+    ClienteSocket(int puerto) {
+        // Crear socket
+        socketCliente = socket(AF_INET, SOCK_STREAM, 0);
+        if (socketCliente == -1) {
+            throw std::runtime_error("Error al crear el socket.");
         }
-        buffer[bytesRecibidos] = '\0';
-        datosRecibidos += buffer;
-    } while (bytesRecibidos == TAM_BUFFER - 1);
 
-    return datosRecibidos;
-}
+        // Configurar dirección del servidor
+        servidorDir.sin_family = AF_INET;
+        servidorDir.sin_port = htons(puerto);             // Puerto del servidor
+        servidorDir.sin_addr.s_addr = inet_addr("127.0.0.1"); // Dirección IP del servidor
 
-// Función para enviar datos al servidor
-bool enviarDatos(int socket, const std::string& datos) {
-    if (send(socket, datos.c_str(), datos.length(), 0) == -1) {
-        std::cerr << "Error al enviar datos al servidor" << std::endl;
-        return false;
-    }
-    return true;
-}
-
-int main() {
-    int socketFd;
-    struct sockaddr_in serverAddress;
-
-    // Crear el socket del cliente
-    socketFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketFd == -1) {
-        std::cerr << "Error al crear el socket" << std::endl;
-        return 1;
+        // Conectar al servidor
+        if (connect(socketCliente, (struct sockaddr *)&servidorDir, sizeof(servidorDir)) == -1) {
+            throw std::runtime_error("Error al conectar al servidor.");
+        }
     }
 
-    // Configurar la dirección del servidor
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
+    std::string recibirDatos() {
+        char buffer[TAM_BUFFER];
+        std::string datosRecibidos;
+        int bytesRecibidos;
 
-    if (inet_pton(AF_INET, SERVER_IP, &(serverAddress.sin_addr)) <= 0) {
-        std::cerr << "Dirección IP inválida o no soportada" << std::endl;
-        return 1;
+        do {
+            bytesRecibidos = recv(socketCliente, buffer, TAM_BUFFER - 1, 0);
+            if (bytesRecibidos == -1) {
+                throw std::runtime_error("Error al recibir datos del servidor");
+            }
+            buffer[bytesRecibidos] = '\0';
+            datosRecibidos += buffer;
+        } while (bytesRecibidos == TAM_BUFFER - 1);
+
+        return datosRecibidos;
     }
 
-    // Conectar con el servidor
-    if (connect(socketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-        std::cerr << "Error al conectar con el servidor" << std::endl;
-        return 1;
+    bool enviarDatos(const std::string &datos) {
+        if (send(socketCliente, datos.c_str(), datos.length(), 0) == -1) {
+            std::cerr << "Error al enviar datos al servidor" << std::endl;
+            return false;
+        }
+        return true;
     }
 
-    // Recibir mensaje de confirmación de conexión
-    char connectMsg[256];
-    int bytesRead = read(socketFd, connectMsg, sizeof(connectMsg) - 1);
-    if (bytesRead < 0) {
-        std::cerr << "Error al recibir la confirmación de conexión" << std::endl;
-        return 1;
+    void cerrarConexion() {
+        close(socketCliente);
     }
-    connectMsg[bytesRead] = '\0';
+};
 
-    std::cout << "Mensaje del servidor: " << connectMsg << std::endl;
-
-    // Recibir mensaje de inicio del juego
-    char startMsg[256];
-    bytesRead = read(socketFd, startMsg, sizeof(startMsg) - 1);
-    if (bytesRead < 0) {
-        std::cerr << "Error al recibir el mensaje de inicio del juego" << std::endl;
-        return 1;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Indicar puerto" << std::endl;
+        return -1;
     }
-    startMsg[bytesRead] = '\0';
 
-    std::cout << "Mensaje del servidor: " << startMsg << std::endl;
+    int serverPort = atoi(argv[1]);
 
+    try {
+        ClienteSocket cliente(serverPort);
 
-    while (true) {
-         // Recibir mensaje de bienvenida
-        std::string mensajeBienvenida = recibirDatos(socketFd);
+        // Recibir mensaje de bienvenida
+        std::string mensajeBienvenida = cliente.recibirDatos();
         std::cout << mensajeBienvenida << std::endl;
 
+        // Recibir y mostrar los tableros inicializados
+        std::string tablerosIniciales = cliente.recibirDatos();
         std::cout << "Tableros iniciales:" << std::endl;
-        std::string tableroString = recibirDatos(socketFd);
-        std::string tableroCPUString = recibirDatos(socketFd);
-        // Solicitar la fila al usuario
-        std::cout << "Ingrese la fila de ataque: ";
-        int fila;
-        std::cin >> fila;
+        std::cout << tablerosIniciales << std::endl;
 
-        // Enviar la fila al servidor
-        if (!enviarDatos(socketFd, std::to_string(fila))) {
-            return 1;
+        while (true) {
+            // Solicitar la fila al usuario
+            std::cout << "Ingrese la fila de ataque: ";
+            int fila;
+            std::cin >> fila;
+
+            // Enviar la fila al servidor
+            if (!cliente.enviarDatos(std::to_string(fila))) {
+                return 1;
+            }
+
+            // Solicitar la columna al usuario
+            std::cout << "Ingrese la columna de ataque: ";
+            int columna;
+            std::cin >> columna;
+
+            // Enviar la columna al servidor
+            if (!cliente.enviarDatos(std::to_string(columna))) {
+                return 1;
+            }
+
+            std::string mensaje = cliente.recibirDatos();
+            std::cout << mensaje << std::endl;
         }
 
-        // Solicitar la columna al usuario
-        std::cout << "Ingrese la columna de ataque: ";
-        int columna;
-        std::cin >> columna;
-
-        // Enviar la columna al servidor
-        if (!enviarDatos(socketFd, std::to_string(columna))) {
-            return 1;
-        }
-
-        // Recibir mensaje de resultado del disparo
-        std::string mensajeResultadoJugador = recibirDatos(socketFd);
-        std::cout << mensajeResultadoJugador << std::endl;
-
-        // Recibir mensaje de resultado del disparo
-        std::string mensajeResultadoCPU = recibirDatos(socketFd);
-        std::cout << mensajeResultadoCPU << std::endl;
-
-        std::string mensajeGanador = recibirDatos(socketFd);
-        std::cout << mensajeGanador << std::endl;
-
-        std::string mensajePerdedor = recibirDatos(socketFd);
-        std::cout << mensajePerdedor << std::endl;
+        cliente.cerrarConexion();
+    } catch (const std::exception &ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
     }
 
-    // Cerrar conexión
-    close(socketFd);
     return 0;
 }
